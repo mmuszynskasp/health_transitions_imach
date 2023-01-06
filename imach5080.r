95a467bcafad90352fb6725fab59d69e6b151434
mmuszynskasp <mmuszynska@gmail.com>
2023-01-06 10:39:57 +0100

    first

--- imach5080.r
+++ imach5080.r
@@ -155,191 +155,151 @@ library(HMDHFDplus)
 library(weights)
 library(anesrake)
 
-
-setwd("K:\\data\\EUROSTAT\\VID_EUSILC\\magda")
+setwd("C:\\Users\\Magdalena\\demography\\withTim\\stateduration\\data\\panel1519")
+#setwd("K:\\data\\EUROSTAT\\VID_EUSILC\\magda")
 mydata <-  read.table(file="longall1519.csv", sep=",",header=TRUE)
 
 
-
-#mutate(ageint=(yearint-yearbirth)*4+(quartint-quartbirh)-200) %>% #age since 50th birthday in quarters
-#         ageint=ifelse(ageint>=120,120,ageint)) %>%
-#  filter(ageint>=0)  #there will be still cases when 2018 interview below 50, it will be deleted in the next step
-
-
+##########################################################################################
+######## we split the data into observation years, as the base weights are originally estimated like this in SILC
 #########################weights readjustments
 #select individuals by their longitudinal paths, in each year-groups-file and for each sex summarize to 100
-indiv1516 <- mydata %>%
-  filter(!is.na(yearint15) & (!is.na(yearint16)|yeardied==2015|(is.na(yearint16) & yeardied==2016))) %>%
+### this implies that we estimate transitions between helath states on average in each single year between 2015-2016
+
+###sorry, it will be a function once I figure out how to paste variable names to insert into functions
+indivall <- mydata %>%
+  filter(!is.na(yearint15)) %>%
   left_join(mydata %>%
-              filter(!is.na(yearint15) & (!is.na(yearint16)|yeardied==2015|(is.na(yearint16) & yeardied==2016))) %>%
+              filter(!is.na(yearint15)) %>%
               group_by(sex) %>%
               dplyr::summarize(sumweights=sum(weight15))) %>%
-  mutate(weightnew=100*weight15/sumweights)
-
-
-indiv1617 <- mydata %>%
-                 filter(!is.na(yearint16) & (!is.na(yearint17)|yeardied==2016|(is.na(yearint17) & yeardied==2017)))%>%
-  left_join(mydata %>%
-              filter(!is.na(yearint16) & (!is.na(yearint17)|yeardied==2016|(is.na(yearint17) & yeardied==2017))) %>%
-                       group_by(sex) %>%
-                       dplyr::summarize(sumweights=sum(weight16))) %>%
-              mutate(weightnew=100*weight16/sumweights)
-
-
-indiv1718 <- mydata %>%
-                 filter(!is.na(yearint17) & (!is.na(yearint18)|yeardied==2017|(is.na(yearint18) & yeardied==2018)))%>%
-  left_join(mydata %>%
-              filter(!is.na(yearint17) & (!is.na(yearint18)|yeardied==2017|(is.na(yearint18) & yeardied==2018))) %>%
-              group_by(sex) %>%
-              dplyr::summarize(sumweights=sum(weight17))) %>%
-  mutate(weightnew=100*weight17/sumweights)
-
-
-indiv1819 <- mydata %>%
-                  filter(!is.na(yearint18) & (!is.na(yearint19)|yeardied==2018|(is.na(yearint19) & yeardied==2019)))%>%
-  left_join(mydata %>% filter(!is.na(yearint18) & (!is.na(yearint19)|yeardied==2018|(is.na(yearint19) & yeardied==2019)))%>%
-              group_by(sex) %>%
-              dplyr::summarize(sumweights=sum(weight18))) %>%
-  mutate(weightnew=100*weight18/sumweights)
-
-indivwall <- indiv1516 %>%   ###the paired records back together
-  add_row(indiv1617 %>%
-            filter(!(indiv1617$iid %in%indiv1516$iid))) %>%
-  add_row(indiv1718 %>%
-            filter(!(indiv1718$iid %in%indiv1516$iid|indiv1718$iid %in%indiv1617$iid))) %>%
-  add_row(indiv1819 %>%
-            filter(!(indiv1819$iid %in%indiv1617$iid|indiv1819$iid %in%indiv1718$iid)))
-
-individ1519 <- indivwall %>%
-  left_join(indivwall %>%
-              group_by(sex) %>%
-              dplyr::summarize(sumweightsall=sum(weightnew)), by=c("sex")) %>% 
-  ###readjust weights for each sex together, to sum to 100 000, data is still wide here
-  mutate(weightnew=10000*weightnew/sumweightsall) %>%
-  select(-c(yearint15,weight15,yearint16,weight16,yearint17,weight17,yearint18,weight18,yearint19,weight19,sumweights,sumweightsall))
-
-
-## finally make the records long and then again wide, since they start at different year
-final1519 <- individ1519 %>%
-  filter(!is.na(quartint15)) %>%
-  mutate(GALI=GALI15,
-         quartint=quartint15,
-         yearint=2015,
-         ageint=(quartint15-quartbirth)+(2015-yearbirth)*4) %>%
-  select(iid,ageint,GALI, quartint,yearint, sex,quartbirth, yearbirth,weightnew) %>%
-  add_row(individ1519 %>%
-            filter(!is.na(quartint16)) %>%
-            mutate(GALI=GALI16,
-                   quartint=quartint16,
-                   yearint=2016,
-                   ageint=(quartint16-quartbirth)+(2016-yearbirth)*4) %>%
-            select(iid,ageint,GALI, quartint,yearint, sex,quartbirth, yearbirth,weightnew)) %>%
-  add_row(individ1519 %>%
-            filter(!is.na(quartint17)) %>%
-            mutate(GALI=GALI17,
-                   quartint=quartint17,
-                   yearint=2017,
-                   ageint=(quartint17-quartbirth)+(2017-yearbirth)*4) %>%
-            select(iid,ageint,GALI, quartint,yearint, sex,quartbirth, yearbirth,weightnew)) %>%
-  add_row(individ1519 %>%
-            filter(!is.na(quartint18)) %>%
-            mutate(GALI=GALI18,
-                   quartint=quartint18,
-                   yearint=2018,
-                   ageint=(quartint18-quartbirth)+(2018-yearbirth)*4) %>%
-            select(iid,ageint,GALI, quartint,yearint, sex,quartbirth, yearbirth,weightnew)) %>%
-  add_row(individ1519 %>%
-            filter(!is.na(quartint19)) %>%
-            mutate(GALI=GALI19,
-                   quartint=quartint19,
-                   yearint=2019,
-                   ageint=(quartint19-quartbirth)+(2019-yearbirth)*4) %>%
-            select(iid,ageint,GALI, quartint,yearint, sex,quartbirth, yearbirth,weightnew)) %>%
-  add_row(individ1519 %>%
-            filter(!is.na(quarterdied)) %>%
-            mutate(GALI=0,
-                   quartint=quarterdied,
-                   yearint=yeardied,
-                   ageint=(quarterdied-quartbirth)+(yeardied-yearbirth)*4) %>%
-            select(iid,ageint,GALI, quartint,yearint, sex,quartbirth, yearbirth,weightnew)) %>%
+  mutate(weightnew=100*weight15/sumweights) %>%
+  mutate(yearint1=yearint15, quartint1=quartint15, GALI1=GALI15, yearint2=yearint16, quartint2=quartint16, GALI2=GALI16,
+         quartint2=ifelse(is.na(quartint2),2,quartint2),
+         quarterdied=ifelse(is.na(quarterdied),2,quarterdied),
+         quartint1=ifelse(is.na(quartint1),2,quartint1),
+         ageint1 = floor(((yearint1-yearbirth)*4+(quartint1-quartbirth))/4),
+         ageint2 = (yearint2-yearbirth)*4+(quartint2-quartbirth),
+         agedied = (yeardied-yearbirth)*4+(quarterdied-quartbirth),
+         ageint2=ifelse(is.na(ageint2),999,ageint2))%>%
+  mutate(GALI2=ifelse((ageint2>=agedied & !is.na(agedied)),0,GALI2),
+         yearint2=ifelse(GALI2==0,yeardied,yearint2),
+         quartint2=ifelse(GALI2==0,quarterdied,quartint2)) %>% 
+  select(c(country:yeardied,sex:quartbirth, weightnew:ageint1)) %>%
+  add_row(mydata %>%
+            filter(!is.na(yearint16)) %>%
+            left_join(mydata %>%
+                        filter(!is.na(yearint16)) %>%
+                        group_by(sex) %>%
+                        dplyr::summarize(sumweights=sum(weight16))) %>%
+            mutate(weightnew=100*weight16/sumweights) %>%
+            mutate(yearint1=yearint16, quartint1=quartint16, GALI1=GALI16, yearint2=yearint17, quartint2=quartint17, GALI2=GALI17,
+                   quartint2=ifelse(is.na(quartint2),2,quartint2),
+                   quarterdied=ifelse(is.na(quarterdied),2,quarterdied),
+                   quartint1=ifelse(is.na(quartint1),2,quartint1),
+                   ageint1 = floor(((yearint1-yearbirth)*4+(quartint1-quartbirth))/4),
+                   ageint2 = (yearint2-yearbirth)*4+(quartint2-quartbirth),
+                   agedied = (yeardied-yearbirth)*4+(quarterdied-quartbirth),
+                   ageint2=ifelse(is.na(ageint2),999,ageint2))%>%
+            mutate(GALI2=ifelse((ageint2>=agedied & !is.na(agedied)),0,GALI2),
+                   yearint2=ifelse(GALI2==0,yeardied,yearint2),
+                   quartint2=ifelse(GALI2==0,quarterdied,quartint2)) %>% 
+            select(c(country:yeardied, sex:quartbirth, weightnew:ageint1))) %>%
+  add_row(mydata %>%
+            filter(!is.na(yearint17)) %>%
+            left_join(mydata %>%
+                        filter(!is.na(yearint17)) %>%
+                        group_by(sex) %>%
+                        dplyr::summarize(sumweights=sum(weight17))) %>%
+            mutate(weightnew=100*weight17/sumweights) %>%
+            mutate(yearint1=yearint17, quartint1=quartint17, GALI1=GALI17, yearint2=yearint18, quartint2=quartint18, GALI2=GALI18,
+                   quartint2=ifelse(is.na(quartint2),2,quartint2),
+                   quarterdied=ifelse(is.na(quarterdied),2,quarterdied),
+                   quartint1=ifelse(is.na(quartint1),2,quartint1),
+                   ageint1 = floor(((yearint1-yearbirth)*4+(quartint1-quartbirth))/4),
+                   ageint2 = (yearint2-yearbirth)*4+(quartint2-quartbirth),
+                   agedied = (yeardied-yearbirth)*4+(quarterdied-quartbirth),
+                   ageint2=ifelse(is.na(ageint2),999,ageint2))%>%
+            mutate(GALI2=ifelse((ageint2>=agedied & !is.na(agedied)),0,GALI2),
+                   yearint2=ifelse(GALI2==0,yeardied,yearint2),
+                   quartint2=ifelse(GALI2==0,quarterdied,quartint2)) %>% 
+            select(c(country:yeardied, sex:quartbirth, weightnew:ageint1))) %>%
+  add_row(mydata %>%
+            filter(!is.na(yearint18)) %>%
+            left_join(mydata %>%
+                        filter(!is.na(yearint18)) %>%
+                        group_by(sex) %>%
+                        dplyr::summarize(sumweights=sum(weight18))) %>%
+            mutate(weightnew=100*weight18/sumweights) %>%
+            mutate(yearint1=yearint18, quartint1=quartint18, GALI1=GALI18, yearint2=yearint19, quartint2=quartint19, GALI2=GALI19,
+                   quartint2=ifelse(is.na(quartint2),2,quartint2),
+                   quarterdied=ifelse(is.na(quarterdied),2,quarterdied),
+                   quartint1=ifelse(is.na(quartint1),2,quartint1),
+                   ageint1 = floor(((yearint1-yearbirth)*4+(quartint1-quartbirth))/4),
+                   ageint2 = (yearint2-yearbirth)*4+(quartint2-quartbirth),
+                   agedied = (yeardied-yearbirth)*4+(quarterdied-quartbirth),
+                   ageint2=ifelse(is.na(ageint2),999,ageint2))%>%
+            mutate(GALI2=ifelse((ageint2>=agedied & !is.na(agedied)),0,GALI2),
+                   yearint2=ifelse(GALI2==0,yeardied,yearint2),
+                   quartint2=ifelse(GALI2==0,quarterdied,quartint2)) %>% 
+            select(c(country:yeardied, sex:quartbirth, weightnew:ageint1))) %>%  
   distinct() %>%
-  mutate(GALI=recode(GALI,'2'='2','1'='2','3'='1','0'='3'),
-         GALI=as.numeric(GALI))%>%
-  filter(ageint>=200 & ageint<320) %>%  #only those aged 50-79.75
-  group_by(iid) %>% 
-  arrange(iid, ageint) %>% 
-  mutate(rank = dense_rank(ageint)) %>% #add the rank of the observation to be able to pivot wider then
-  pivot_wider(names_from = rank, values_from = ageint:yearint)
+  mutate(agedied=floor(((yeardied-yearbirth)*4+(quarterdied-quartbirth))/4), #age at interview is age in completed years, 
+    agedied=ifelse(GALI2==0,agedied,NA),
+    GALI1=recode(GALI1,'2'='2','1'='2','3'='1','0'='3'),
+         GALI2=recode(GALI2,'2'='2','1'='2','3'='1','0'='3'),
+         GALI1=as.numeric(GALI1), GALI2=as.numeric(GALI2))%>%
+  filter(ageint1>=50 & ageint1<80) %>% #only those aged 50-79 at 1st interview
+  mutate(ageint5=5*floor(ageint1/5),
+         agedied=ifelse(agedied>79,NA,agedied),
+         survived=ifelse(!is.na(agedied),0,1)) %>%
+  filter(!is.na(yearint2)) #remove those who attrited between the interviews
+
+
+survprob <- indivall %>%
+  filter(survived==1) %>% 
+  group_by(sex,ageint5) %>%
+  summarise(survived=sum(weightnew)) %>%
+  left_join(indivall %>% 
+              group_by(sex,ageint5) %>%
+              summarise(pop=sum(weightnew))) %>%
+  mutate(probd=1-survived/pop)
   
-write.table(final1519, file="final1519.csv", sep=",",row.names=FALSE)
+
+### deaths by 5-year age groups
+males <-  survprob %>%
+  filter(sex==1)
+
+qx <- c(0.01521, 0.02497,  0.04082, 0.06357, 0.10384, 0.16879) #from HMD, Italian males, 2017, 5-year age groups
+qx <- c(0.01499, 0.02465,  0.03992, 0.06313, 0.10189, 0.16677) #from HMD, Italian males, 2015-2019, 5-year age groups
 
 
-######################################################################
-dev.off()
-rm(list=ls())
+calibrmarg <- cbind(males,qx) %>%
+  mutate(wmulti=qx/probd)
 
-library(tidyr)
-library(dplyr)
-
-setwd("K:\\data\\EUROSTAT\\VID_EUSILC\\magda")
-###check deaths by age
-males <-  read.table(file="final1519.csv", sep=",",header=TRUE) %>%
-  filter(sex==1) %>%
-  mutate(agedeath2=ifelse(GALI_2==3, 5*floor(ageint_2/20),0),
-         agedeath3=ifelse(GALI_3==3,5*floor(ageint_3/20),0),
-         agedeath4=ifelse(GALI_4==3,5*floor(ageint_4/20),0)) %>%
-  dplyr::mutate(across(starts_with("agedeath"), ~replace_na(.,0))) %>%
-  mutate(agedeath=as.numeric(agedeath2)+as.numeric(agedeath3)+as.numeric(agedeath4)) %>%
-  mutate(age1= 5*floor(ageint_1/20),
-         cases=1,
-         cases=ifelse(is.na(ageint_2),0,1)) 
-
-deathprob <- males %>%
-  mutate(death=ifelse(agedeath>0,1,0)) %>%
-  group_by(age1) %>%
-    summarise(deaths=sum(death),startpop=sum(cases))
-
-qx <- c(0.01499, 0.02465,0.03992, 0.06313, 0.10189, 0.16677) #from HMD, Italian males, 2015-2019, 5-year age groups
-expected <- round(9/24*qx*deathprob$startpop)
-
-brutald <- cbind(deathprob,expected) %>%
-  mutate(wmulti=expected/deaths)
-
-forimach <- read.table(file="final1519.csv", sep=",",header=TRUE) %>%
- mutate(age1=5*floor(ageint_1/20)) %>%
- left_join(brutald, by="age1") %>%
- mutate(weightnew2=ifelse(GALI_2==3,weightnew*wmulti,NA),
-         weightnew3=ifelse(GALI_3==3,weightnew*wmulti,NA),
-         weightnew4=ifelse(GALI_4==3,weightnew*wmulti,NA)) %>%
-  mutate(weightnew=ifelse(!is.na(weightnew2),weightnew2,weightnew),
-         weightnew=ifelse(!is.na(weightnew3),weightnew3,weightnew),
-         weightnew=ifelse(!is.na(weightnew4),weightnew4,weightnew)) %>%
-  dplyr:: mutate_at(vars(starts_with("quart")), ~replace_na(.,99)) %>%
+forimach <- indivall %>%
+ filter(sex==1) %>%
+ left_join(calibrmarg %>% 
+             select(ageint5,wmulti), by="ageint5") %>%
+  mutate(weightnew=ifelse(GALI2==3,weightnew*wmulti/4.75,weightnew)) %>%
   dplyr::mutate_at(vars(starts_with("quart")), ~recode(.,'1'='2','2'='5','3'='8','4'='11','99'='99')) %>% #recode from quarters to months
   dplyr::mutate(across(starts_with("yearint"), ~replace_na(.,9999))) %>% 
   dplyr::mutate(across(starts_with("GALI"), ~replace_na(.,-1))) %>% 
-  filter(GALI_1!=3 & GALI_1!=-1) %>% #remove the case where the first observation is dead or NA
   mutate(birth=paste(quartbirth,yearbirth,sep="/"),
-         int1=paste(quartint_1,yearint_1,sep="/"),
-         int2=paste(quartint_2,yearint_2,sep="/"),
-         int3=paste(quartint_3,yearint_3,sep="/"),
-         int4=paste(quartint_4,yearint_4,sep="/"),
+         int1=paste(quartint1,yearint1,sep="/"),
+         int2=paste(quartint2,yearint2,sep="/"),
          death="99/9999",
-         death=ifelse(GALI_1==3,int1,death),
-         death=ifelse(GALI_2==3,int2,death),
-         death=ifelse(GALI_3==3,int3,death),
-         death=ifelse(GALI_4==3,int4,death)) %>%
+         death=ifelse(GALI2==3,int2,death)) %>%
   # mutate(int5="1/2020",
   #        GALI_5=-2,
   #   #     GALI_5=ifelse(death!="99/9999",3,-2),
   #        GALI_4=ifelse(int4=="99/9999",GALI_5,GALI_4),
   #        int4=ifelse(int4=="99/9999",int5,int4))%>%
 #  dplyr::mutate_at(vars(starts_with("GALI")), ~ifelse(.==3,-1,.)) %>%
- select(sex,weightnew,birth,death,int1,GALI_1,int2,GALI_2,int3,GALI_3,int4,GALI_4)
+ select(weightnew,birth,death,int1,GALI1,int2,GALI2)
 
 
-ourdata <- forimach %>%
-  filter(sex==1)
+ourdata <- forimach 
 rownames(ourdata) <- 1:nrow(ourdata)
 write.table(ourdata, file="forimach.txt", col.names=FALSE,quote=FALSE)
 
